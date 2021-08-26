6d244745c50f53ee15dd704fa732102e731ec3a6
calfcrusher <calfcrusher@inventati.org>
2021-08-26 15:15:46 +0200

    Update ZipBrute.py

--- ZipBrute.py
+++ ZipBrute.py
@@ -1,68 +1,89 @@
-# Multi-Threaded ZIP files bruteforce tool
+# A simple bruteforce tool to crack UNIX passwords using wordlists
 # calfcrusher@inventati.org
-# Usage: ./ZipBrute.py -f file.zip -d wordlist.txt
+# Usage: ./ShadowBrute.py HASHFILE WORDLIST
+# insert many hash as you want in HASHFILE once one per line, same in wordlist file
 
-import optparse
-import threading
-import zipfile
+import crypt
+import sys
 import time
 import pyfiglet
 import os
 import psutil
 
 
-def extractzip(zipfilepath, password, startedtime):
-    """Extract zip file"""
+def crackpass(list, hashstring, salted):
+    """Bruteforce function"""
 
     try:
-        with zipfile.ZipFile(zipfilepath) as file:
-            file.extractall(pwd=str.encode(password))
-            print("[+] PASSWORD FOUND -> " + password)
-            print("[+] Files extracted in current directory")
-            print('It took {0:0.1f} seconds'.format(time.time() - startedtime))
-            # Terminate process pid to kill all threads
-            current_system_pid = os.getpid()
-            terminator = psutil.Process(current_system_pid)
-            terminator.terminate()
-    except BaseException:
-        pass
+        wlist = open(list, 'r')
+    except:
+        print("Error: Unable to access wordlist file")
+        exit(0)
+
+    # Check if salt is present
+    if salted:
+        for word in wlist:
+            word = word.strip('\n')
+            original_hash = str(salted + "$" + hashstring).strip('\n')
+            crypted_word = crypt.crypt(word, salt=salted)
+            if original_hash == crypted_word.strip('\n'):
+                print("[+] HASH: " + salted + hashstring)
+                print("[+] CRACKED: " + word + "\n")
+                break
+    else:
+        for word in wlist:
+            word = word.strip('\n')
+            crypted_word = crypt.crypt(word)
+            if hashstring == crypted_word.strip('\n'):
+                print("[+] HASH: " + hashstring)
+                print("[+] CRACKED: " + word + "\n")
+                break
+
+    wlist.close()
 
 
 def main():
     """Main function of tool"""
 
-    ascii_banner = pyfiglet.figlet_format("ZipBrute")
+    ascii_banner = pyfiglet.figlet_format("ShadowBrute")
     print(ascii_banner)
     print("calfcrusher@inventati.org | For educational use only.")
     print("*****************************************************\n")
 
-    start = time.time()
-
-    parser = optparse.OptionParser("./ZipBrute.py -f <zipfile> -d <wordlist>")
-    parser.add_option('-f', dest='filezip', type='string', help='specify zip file')
-    parser.add_option('-d', dest='wordlist', type='string', help='specify dictionary file')
-
-    (options, args) = parser.parse_args()
-
-    if options.filezip is None or options.wordlist is None:
-        print(parser.usage)
+    if len(sys.argv) < 3 or len(sys.argv) > 3:
+        print("Usage: ./ShadowBrute.py hashes.txt wordlist.txt")
         exit(0)
     else:
-        pathzip = options.filezip
-        wordlist = options.wordlist
+        filehash = sys.argv[1]
+        wordlist = sys.argv[2]
+        try:
+            fhash = open(filehash, 'r')
+        except:
+            print("Error: Unable to access hash file")
+            exit(0)
 
-    if zipfile.is_zipfile(pathzip):
-        print('running...')
-        with open(wordlist) as wlist:
-            for line in wlist.readlines():
-                password = line.strip('\n')
-                t = threading.Thread(target=extractzip, args=(pathzip, password, start))
-                t.start()
 
-    print('It took {0:0.1f} seconds'.format(time.time() - startedtime))
-    
-if __name__ == '__main__':
+    # Reading lines from hash file
+    for line in fhash.readlines():
+        # Check if our hash contains salt or not
+        if str(line).count('$') == 3:
+            hash_identifier = str(line.split('$')[1])
+            # We construct the salt in this way: $1$saltstring
+            salt = str("$" + hash_identifier + "$" + (line.split('$')[2]))
+            hash = str(line.split ('$')[3])
+        elif str(fhash).count('$') == 2:
+            hash = line.split('$')[2]
+            salt = 0
+        else:
+            print("Error: Unrecognized hash in " + filehash)
+            exit(0)
+
+        crackpass(wordlist, hash, salt)
+
+    fhash.close()
+
+
+if __name__ == "__main__":
+    start = time.time()
     main()
-
-print("No valid password found!")
-
+    print('It took {0:0.1f} seconds'.format(time.time() - start))
